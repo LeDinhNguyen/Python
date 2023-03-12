2f4ca15720697c901e022bbcdc83058692b88e39
Le Dinh Nguyen <ledinhnguyen00@gmail.com>
2023-03-12 23:02:36 +0700

    update check ascending number input string

--- PFP191/Exam/PracticalExam/checkAscendingIntegerOfString.py
+++ PFP191/Exam/PracticalExam/checkAscendingIntegerOfString.py
@@ -1,4 +1,4 @@
-def findIntegerOfString(string: str):
+def findIntegerOfString(string: str) -> list:
     found = "0"
     sign = 1
     num_list = []
@@ -7,32 +7,40 @@ def findIntegerOfString(string: str):
         if ch == "-":
             sign = -1
         elif ch.isnumeric():
-            found += ch
+            if found == "0" and ch == "0":
+                num_list.append(int(ch))
+            else:
+                found += ch
         else:
             cur_num = int(found) * sign
-            num_list.append(cur_num)
+            if cur_num != 0:
+                num_list.append(cur_num)
 
             if sign == -1:
                 sign = 1
 
             found = "0"
+    cur_num = int(found) * sign
+    if cur_num != 0:
+        num_list.append(cur_num)
+    print(num_list)
     return num_list
 
 
 def checkAscending(string: str) -> bool:
     num_list = findIntegerOfString(string)
     pre_num = num_list[0]
-    flag = 0
-    while flag == 0:
-        for i in range(1, len(num_list)):
-            if num_list[i] > pre_num:
-                flag += 1
-    return flag != 0
+    for i in range(1, len(num_list)):
+        if num_list[i] >= pre_num:
+            pre_num = num_list[i]
+        else:
+            return False
+    return True
 
 
 def main():
-    print(checkAscending("-12d-1njda-3"))
-
+    string = input("Enter the string: ")
+    print(checkAscending(string))
 
 if __name__ == "__main__":
     main()
